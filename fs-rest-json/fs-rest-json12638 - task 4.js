1cd95e4049ea2205ebd491e44a35e8638ca502ae
Irina Novik <solvejg.23@gmail.com>
2020-10-23 19:25:05 +0300

    add another version with the method Array.prototype.find()

--- fs-rest-json/fs-rest-json12638 - task 4.js
+++ fs-rest-json/fs-rest-json12638 - task 4.js
@@ -3,21 +3,29 @@
 const fetch = require("node-fetch");
 const REQUESTED_URL = "http://www.groupkt.com/country/get/all";
 
-function printIsoCodesOfCountry(url, chosenCountry) {
-    fetch(url)
+function printIsoCodesOfCountry(chosenCountry) {
+    fetch(REQUESTED_URL)
     .then(response => {
         if (response.ok) {
             return response.json();
         }
     })
     .then(json => {
-        json.RestResponse.result.forEach(country => {
-            if (country.name === chosenCountry) {
-                console.log(`\nISO codes of ${chosenCountry}: ${country.alpha2_code} and ${country.alpha3_code}.\n`);
+        let countrySearchResult = json.RestResponse.result.find(country => {
+            if (country.name === chosenCountry){
+                return country;
             }
-        });
+        })
+
+        if (countrySearchResult === undefined) {
+            throw new Error(`There is no such a country in the list: ${chosenCountry}`);
+        } else {
+            console.log(`\nISO codes of ${chosenCountry}: ${countrySearchResult.alpha2_code}`
+                            + ` and ${countrySearchResult.alpha2_code}.\n`);
+        }
     })
-    .catch(error => console.error(`\n(!) Network request for ${REQUESTED_URL} failed with error:\n ${error}.\n`));
+    .catch(error => console.error(`\n(!) Network request for ${REQUESTED_URL}`
+                                    + ` failed with error:\n ${error}.\n`));
 }
 
-printIsoCodesOfCountry(REQUESTED_URL, "Norway");
+printIsoCodesOfCountry("Norway");
